5c0a73fd0e7c59b14a7fef3ec04eae0bd68235b4
Rishabh Thakur <rishabh@Rishabhs-MacBook-Pro.local>
2020-05-25 01:52:06 +0530

    Taking in location from terminal

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
+const yargs = require("yargs");
 
-geocode("Bengaluru", (error, data) => {
-    if (error) {
-        return console.log(error);
-    }
-    forecast(data, (error, response) => {
+const address = process.argv[2];
+
+if (address) {
+    geocode(process.argv[2], (error, data) => {
         if (error) {
-            console.log(error);
-        } else {
-            console.log(data.location);
-            console.log(response);
+            return console.log(error);
         }
+        forecast(data, (error, response) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(data.location);
+                console.log(response);
+            }
+        });
     });
-});
+}
+else{
+    console.log("Please provide an address");
+}
